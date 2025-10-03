588fd969c0b80945113c4a489e6eba3f0936e415
Catherine <catherinecharlesfesto@gmail.com>
2025-10-03 19:34:31 +0300

    m

---
+++ main2.py
@@ -0,0 +1,7 @@
+#Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}
+colour1={"green","blue"}
+colour2={"blue","yellow"}
+colour3=colour1.intersection(colour2)
+colour4=colour1.union(colour2)
+print(colour3)
+print(colour4)
