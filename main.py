b5f299e247a0580defea2c0e68fbc1e1981116ba
Catherine <catherinecharlesfesto@gmail.com>
2025-10-03 19:25:15 +0300

    m

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#Write a program to perform the following operations: 1. Create a tuple with different datatypes 2. Create another tuple of integers 3. Create a new tuple by adding 9 to the previous tuple 4. Count the occurrences of an element in the tuple 5. Perform slicing on the tuple
-#Create a tuple with different datatypes
-tuple1=(10,"catherine", 3.14, True)
-print("my tuple:", tuple1)
-tuple2=(5,6,7,8,10)
-tuple2=tuple2+(9,)
-print("the tuple after adding 9 is:",tuple2)
-tuple3=(10,10,20,50,10,30,40)
-print("the count of 10 in the tuple is:",tuple1.count(10))
-print("from tuple 3 :",tuple3[1:4])
+#Write a program to create a set and perform the following operations on that set- 1. Create a set with integer elements 2. Create a set with mixed data type elements 3. Create another set with elements - 1, 2, 3, 4, 3, 2 4. Create a set from a list with elements - [1, 2, 3, 2] 5. Print the set after removing the first element from this set - [0, 1, 3, 4, 5]
+a=(1,2,3,4,5)
+b={1,2,3,"hello",False}
+print(b)
+a.add(8)
+print(a)
+c={1,2,3,4,3,2}
+print(c)
+d=set([1,2,3,2])
+print(d)
