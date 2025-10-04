f55214bb3a09efa9e55a9ff167924c9121d94b1e
Catherine <catherinecharlesfesto@gmail.com>
2025-10-04 17:34:38 +0300

    m

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-#Write a program to create a set and perform the following operations on that set- 1. Create a set with integer elements 2. Create a set with mixed data type elements 3. Create another set with elements - 1, 2, 3, 4, 3, 2 4. Create a set from a list with elements - [1, 2, 3, 2] 5. Print the set after removing the first element from this set - [0, 1, 3, 4, 5]
-a=(1,2,3,4,5)
-b={1,2,3,"hello",False}
-print(b)
-a.add(8)
-print(a)
-c={1,2,3,4,3,2}
+#Write a program to return - 1. zipped list from two lists 2. elements of two lists zipped together, but 2nd list in reverse order 3. elements of two lists zipped into a dictionary
+a=["Catherine","jisoo","lisa","rose","jennie"]
+b=[1,2,3,4,5,6]
+c=list(zip(a,b))
 print(c)
-d=set([1,2,3,2])
-print(d)
--- main3.py
+++
@@ -1,6 +0,0 @@
-import array as arr
-#Write a program to create an array with the following elements - [1, 3, 5, 3, 7, 9, 3]. Then find the number of occurrences of number 3 in the array. Also, print the reversed array.
-arr_num= arr.array('i',[1, 3, 5, 3, 7, 9, 3])
-print("The number of times 3 occurs in the array is ",arr_num.count(3))
-arr_num.reverse()
-print(str(arr_num))
